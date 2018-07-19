80a7fd327456cce6c713208e5189625b44066296
William Ayerst <williamayerst@AML0131.local>
2018-07-19 11:01:05 +0100

    Initial code commit
    Resetting author

---
+++ var.tf
@@ -0,0 +1,31 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agent_count" {
+  default = 2
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+  default = "aks"
+}
+
+variable "tags" {
+  type    = "map"
+  default = {}
+}
+
+variable cluster_name {
+  default = "akscluster"
+}
+
+variable resource_group_name {
+  default = "akscluster"
+}
+
+variable location {
+  default = "westeurope"
+}
